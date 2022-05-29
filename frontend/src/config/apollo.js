b457994136f139cf7c5e1b1e5033c3341d8ab07e
luis <luis.aguayo@dynamicsolutions.com.mx>
2022-05-29 12:41:34 -0500

    Change url prod

--- frontend/src/config/apollo.js
+++ frontend/src/config/apollo.js
@@ -3,8 +3,8 @@ import { createUploadLink } from "apollo-upload-client";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createUploadLink({
-  // uri: "http://127.0.0.1:4000/graphql", //LOCALHOST
-  uri: "http://34.236.150.231/graphql/", //PROD
+  uri: "http://127.0.0.1:4000/graphql", //LOCALHOST
+  // uri: "http://34.236.150.231/graphql", //PROD
 });
 
 const authLink = setContext((_, { headers }) => {
